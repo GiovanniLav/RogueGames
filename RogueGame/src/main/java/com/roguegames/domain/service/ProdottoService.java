aefab46b12fa572adfe60ff80b3174242b759eb5
Marco <m.battaglia13@studenti.unisa.it>
2025-01-14 18:28:19 +0100

    aggiunta del catalogo

---
+++ RogueGame/src/main/java/com/roguegames/domain/service/ProdottoService.java
@@ -0,0 +1,26 @@
+package com.roguegames.domain.service;
+
+import com.roguegames.domain.entity.Prodotto;
+import com.roguegames.domain.repository.ProdottoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.Optional;
+import java.util.List;
+
+@Service
+public class ProdottoService {
+
+    @Autowired
+    private ProdottoRepository prodottoRepository;
+
+    public List<Prodotto> getAllProdotti() {
+        return prodottoRepository.findAll();
+    }
+
+    public Prodotto saveProdotto(Prodotto prodotto) {
+        return prodottoRepository.save(prodotto);
+    }
+}
