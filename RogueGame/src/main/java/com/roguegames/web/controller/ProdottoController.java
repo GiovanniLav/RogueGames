86ff455cf6dd312459ebc6adfcd3588fa2aa2626
GiovanniLav <g.lavorato@studenti.unisa.it>
2025-01-16 12:24:06 +0100

    Catalogo funzionante

--- RogueGame/src/main/java/com/roguegames/web/controller/ProdottoController.java
+++ RogueGame/src/main/java/com/roguegames/web/controller/ProdottoController.java
@@ -2,8 +2,6 @@ package com.roguegames.web.controller;
 import com.roguegames.domain.entity.Prodotto;
 import com.roguegames.domain.service.ProdottoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
